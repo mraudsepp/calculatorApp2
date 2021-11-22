fcab72c2e7881c8ea42f874ab2747f12fc626814
mraudsepp <martraudsepp1@hotmail.com>
2021-11-22 12:57:25 +0200

    a

---
+++ lib/second_Screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+
+class SecondRoute extends StatefulWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+
+  final _unitController1 = TextEditingController();
+  final _unitController2 = TextEditingController();
+  var _tulemus1 = ''; var _tulemus2 = '';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Konverter"),
+      ),
+body: Column(children: <Widget> [
+  Padding(
+    padding: const EdgeInsets.all(10),
+    child: Text('Convert km to miles',
+    style: TextStyle(fontSize: 20, color: Colors.grey)),
+  ),
+  Padding(
+    padding: const EdgeInsets.all(10),
+    child: TextField(
+      keyboardType: TextInputType.number,
+     controller: _unitController1,
+    ),
+  ),
+
+  Padding(
+    padding: const EdgeInsets.all(10),
+    child: Text('Convert miles to km',
+        style: TextStyle(fontSize: 20, color: Colors.grey)),
+  ),
+  Padding(
+    padding: const EdgeInsets.all(10),
+    child: TextField(
+      keyboardType: TextInputType.number,
+      controller: _unitController2,
+    ),
+  ),
+
+  ElevatedButton(child: Text('Konverteeri'),
+      onPressed: (){
+    setState(() {
+      _tulemus1 = '${int.parse(_unitController1.text) * 0.621371}';
+      _tulemus2 = '${int.parse(_unitController2.text) * 1.60934}';
+    });
+
+  }
+  ),
+  SizedBox(height: 20,),
+  Text(_tulemus1,),
+  Text(_tulemus2),
+  ElevatedButton(child: Text('Tühista'),
+      onPressed: (){
+        setState(() {
+          _tulemus1 = '';
+          _tulemus2 = '';
+        });
+
+      }
+  ),
+
+],
+),
+    );
+  }
+}
+
